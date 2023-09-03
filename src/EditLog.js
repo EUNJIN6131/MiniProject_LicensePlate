080574d7a7adf09ffbe6d614a358868a2e5cd759
EUNJIN6131 <129818813+EUNJIN6131@users.noreply.github.com>
2023-09-03 16:24:57 +0900

    0903

--- src/EditLog.js
+++ src/EditLog.js
@@ -1,10 +1,19 @@
+import * as React from "react";
 import { Box } from "@mui/material";
 
-export default function EditLog() {
-
-    return(
-        <Box>
-            d
-        </Box>
+export default function EditLog({ isModificationLogVisible,
+    licensePlateBeforeModification,
+    licensePlateAfterModification,
+    modificationDateTime, }) {
+    return (
+        <>
+            {isModificationLogVisible && (
+                <Box>
+                    <p>수정 전 : {licensePlateBeforeModification}</p>
+                    <p>수정 후 : {licensePlateAfterModification}</p>
+                    <p>수정 일시 : {modificationDateTime}</p>
+                </Box>
+            )}
+        </>
     );
 }
