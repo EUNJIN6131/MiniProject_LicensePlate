f2766246516188567242589c518571b2690676da
EUNJIN6131 <hailey6131@gmail.com>
2023-09-11 17:44:42 +0900

    0911

--- src/PaginationButtons.js
+++ src/PaginationButtons.js
@@ -8,25 +8,20 @@ export default function PaginationButtons({ currentPage, onPageChange, pageCount
 
   const [isAdmin, setIsAdmin] = useState(false);    
 
-  console.log("isAdmin:", isAdmin); // isAdmin 값 출력
-
   useEffect(() => {
     findIsAdmin(); // 컴포넌트가 처음 렌더링 될 때 isAdmin 값을 설정
   }, []);
 
   const findIsAdmin = () => {
     const jwtToken = localStorage.getItem("ACCESS_TOKEN");
-    console.log("jwtToken", jwtToken)
     if (jwtToken) {
       // JWT 토큰이 존재한다면 디코딩하여 Role을 확인
       const decodedToken = jwt_decode(jwtToken);
       if (decodedToken.auth === "ROLE_ADMIN") {
         setIsAdmin(true);
-        console.log("decodedToken.role", decodedToken.role);
+
       } else {
         setIsAdmin(false);
-        console.log("False", decodedToken.role)
-        console.log(decodedToken)
       }
     } else {
       setIsAdmin(false);
